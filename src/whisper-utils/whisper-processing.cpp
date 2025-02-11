88cb2672e2ec13c9cb7b801b0f090a5947d56138
rabidcopy <rabidcopy@yahoo.com>
2025-02-10 19:01:47 -0600

    Update whisper-processing.cpp

--- src/whisper-utils/whisper-processing.cpp
+++ src/whisper-utils/whisper-processing.cpp
@@ -57,16 +57,16 @@ struct whisper_context *init_whisper_context(const std::string &model_path_in,
 		gf);
 
 	struct whisper_context_params cparams = whisper_context_default_params();
-#elif defined(LOCALVOCAL_WITH_VULKAN)
+#ifdef	LOCALVOCAL_WITH_VULKAN
 	cparams.use_gpu = true;
 	obs_log(LOG_INFO, "Using Vulkan for inference");
-#ifdef LOCALVOCAL_WITH_CUDA
+#elif	defined(LOCALVOCAL_WITH_CUDA)
 	cparams.use_gpu = true;
 	obs_log(LOG_INFO, "Using CUDA GPU for inference, device %d", cparams.gpu_device);
-#elif defined(LOCALVOCAL_WITH_HIPBLAS)
+#elif	defined(LOCALVOCAL_WITH_HIPBLAS)
 	cparams.use_gpu = true;
 	obs_log(LOG_INFO, "Using hipBLAS for inference");
-#elif defined(__APPLE__)
+#elif	defined(__APPLE__)
 	cparams.use_gpu = true;
 	obs_log(LOG_INFO, "Using Metal/CoreML for inference");
 #else
